9ef380ddbf461dd047bae413e368412c1a0b5da0
tri <bienthanhtri@gmail.com>
2023-09-06 14:28:38 +0700

    Init state value with function

--- src/Example1.js
+++ src/Example1.js
@@ -1,7 +1,23 @@
 import { useState } from "react"
 
+
+const data = [1, 2, 5]
+
 function Example1() {
     const [counter, setCounter] = useState(1)
+
+    // we can initialize the value for useState with a function 
+    // its return value will be assigned to the state
+    // it only run ONCE
+    const [value, setValue] = useState(() => {
+        // do another stuff here
+        console.log("calc initial value ONCE")
+        return data.reduce((acc, cur) => acc + cur, 0)
+    })
+
+    const handleValue = () => {
+        setValue(value + 1)
+    }
 
     const handleClick = () => {
         // simple way
@@ -11,12 +27,14 @@ function Example1() {
         setCounter(prev => prev + 1)
     }
 
-    console.log("re-render")
     return (
         <div>
             <h2>Example 1: Counter with useState()</h2>
             <p>Counter: {counter} </p>
             <button onClick={handleClick}>Add 1</button>
+
+            <p>Value: {value}</p> 
+            <button onClick={handleValue}>Value + 1</button>
         </div>
     )
 }
